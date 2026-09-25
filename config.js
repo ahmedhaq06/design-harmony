df81d4d9f444c157ca48f6657ddf3b70bb2117e2
Ahmed <ahmedarshadul23@gmail.com>
2026-09-25 20:13:23 +0530

    phone number form fix

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.ENV = {
   SUPABASE_URL: "https://jdkrisfxkegywsyhqkpj.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impka3Jpc2Z4a2VneXdzeWhxa3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNzQ1NzcsImV4cCI6MjEwNTg1MDU3N30.o6rXEm_CxLNhQd_n0-0Lp72ym2gPJvFx8FE0MD3e_og",
-  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbx5hI1g1fD-3WBOHe2S85ehVBS6SliAgi6ZwiMMl-YVUVPLKXomEaUDlKxGkjxRyAv-/exec"
+  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbwKr12Lw4uW2aQLaNjtHLGC-vGmXL29_w8zHGvq0LDqy9R9ocsrG94uYC-sXU8x6O_m0g/exec"
 };
