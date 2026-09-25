8da79383009a5be7f64f159bb7c088cc3a804777
Ahmed <ahmedarshadul23@gmail.com>
2026-09-25 17:32:31 +0530

    admin page error v2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Local environment configuration for Design Harmony
 window.ENV = {
   SUPABASE_URL: "https://jdkrisfxkegywsyhqkpj.supabase.co",
-  SUPABASE_ANON_KEY: "sb_secret_EOKOmYPR_GH_OEYEHeQXkw_VrIYPFX6",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impka3Jpc2Z4a2VneXdzeWhxa3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNzQ1NzcsImV4cCI6MjEwNTg1MDU3N30.o6rXEm_CxLNhQd_n0-0Lp72ym2gPJvFx8FE0MD3e_og",
   GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbxjjubnwQsSGE06hX_8X_N2FFvSU11878rHTu0NluENBWIgj4nKxUIgrYobUU2ydDM5/exec"
 };
