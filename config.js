bb3ac2c0cf584d5202e052f8cc1d2fe7bb083698
Ahmed <ahmedarshadul23@gmail.com>
2026-09-25 19:26:41 +0530

    form

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.ENV = {
   SUPABASE_URL: "https://jdkrisfxkegywsyhqkpj.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impka3Jpc2Z4a2VneXdzeWhxa3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNzQ1NzcsImV4cCI6MjEwNTg1MDU3N30.o6rXEm_CxLNhQd_n0-0Lp72ym2gPJvFx8FE0MD3e_og",
-  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbxjjubnwQsSGE06hX_8X_N2FFvSU11878rHTu0NluENBWIgj4nKxUIgrYobUU2ydDM5/exec"
+  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbx5hI1g1fD-3WBOHe2S85ehVBS6SliAgi6ZwiMMl-YVUVPLKXomEaUDlKxGkjxRyAv-/exec"
 };
